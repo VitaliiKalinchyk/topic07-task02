098c74e6a4bfdef41613fc6a8903582d6a87bdcd
Vitalii Kalinchyk <karlkory@gmail.com>
2022-10-22 16:07:30 +0300

    23 10

--- src/main/java/com/epam/rd/java/basic/topic07/task02/db/entity/Team.java
+++ src/main/java/com/epam/rd/java/basic/topic07/task02/db/entity/Team.java
@@ -3,7 +3,41 @@ package com.epam.rd.java.basic.topic07.task02.db.entity;
 public class Team {
 
 	private int id;
-		
-	private String name;
 
+	private final String name;
+
+
+	private Team(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public static Team createTeam(String name) {
+		return new Team(0, name);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Team team = (Team) o;
+		return name.equals(team.name);
+	}
 }
